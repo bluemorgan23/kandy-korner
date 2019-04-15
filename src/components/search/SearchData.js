da5407af798e9d0c674bd4d48adb07a2d96dc72c
Chris Morgan <bluemorgan2323@gmail.com>
2019-04-15 10:37:33 -0500

    Started working on search bar. Incomplete.

--- src/components/search/SearchData.js
+++ src/components/search/SearchData.js
@@ -3,6 +3,9 @@ import React, { Component } from "react"
 
 class SearchData extends Component {
 
+
+
+
     handleKeyDown(KeyboardEvent) {
         if (KeyboardEvent.key === "Enter"){
             console.log("test");
